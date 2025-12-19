d6c0313c07cafde72bbadaa26c34330d3afa0b8e
Yash Patel <yashpatel6022@gmail.com>
2025-12-19 17:02:04 +0530

    top.sv

--- top.sv
+++ top.sv
@@ -3,47 +3,47 @@
 module top;
   bit clk_1,rstn_1;
   bit clk_2,rstn_2;
-  mem_intf pif_1(clk_1,rstn_1,clk_2,rstn_2);
-  mem_intf pif_2(clk_1,rstn_1,clk_2,rstn_2);
+  mem_intf pif(clk_1,rstn_1,clk_2,rstn_2);
+  //mem_intf pif(clk_1,rstn_1,clk_2,rstn_2);
   
   always #5 clk_1=~clk_1;
   always #5 clk_2=~clk_2;
   
   memory #(`width,`depth,`addr_width) dut(
-    .clk_1(pif_1.clk_1),
-    .rstn_1(pif_1.rstn_1),
-    .write_1(pif_1.write_1),
-    .valid_1(pif_1.valid_1),
-    .wdata_1(pif_1.wdata_1),
-    .rdata_1(pif_1.rdata_1),
-    .addr_1(pif_1.addr_1),
-    .ready_1(pif_1.ready_1),
+    .clk_1(pif.clk_1),
+    .rstn_1(pif.rstn_1),
+    .write_1(pif.write_1),
+    .valid_1(pif.valid_1),
+    .wdata_1(pif.wdata_1),
+    .rdata_1(pif.rdata_1),
+    .addr_1(pif.addr_1),
+    .ready_1(pif.ready_1),
     
     //Port 2
-    .clk_2(pif_2.clk_2),
-    .rstn_2(pif_2.rstn_2),
-    .write_2(pif_2.write_2),
-    .valid_2(pif_2.valid_2),
-    .wdata_2(pif_2.wdata_2),
-    .rdata_2(pif_2.rdata_2),
-    .addr_2(pif_2.addr_2),
-    .ready_2(pif_2.ready_2));
+    .clk_2(pif.clk_2),
+    .rstn_2(pif.rstn_2),
+    .write_2(pif.write_2),
+    .valid_2(pif.valid_2),
+    .wdata_2(pif.wdata_2),
+    .rdata_2(pif.rdata_2),
+    .addr_2(pif.addr_2),
+    .ready_2(pif.ready_2));
   
   assertions #(`width,`depth,`addr_width)uut(
-    .clk(pif_1.clk_1),
-    .rstn(pif_1.rstn_1),
-    .write(pif_1.write_1),
-    .valid(pif_1.valid_1),
-    .wdata(pif_1.wdata_1),
-    .rdata(pif_1.rdata_1),
-    .addr(pif_1.addr_1),
-    .ready(pif_1.ready_1));
+    .clk(pif.clk_1),
+    .rstn(pif.rstn_1),
+    .write(pif.write_1),
+    .valid(pif.valid_1),
+    .wdata(pif.wdata_1),
+    .rdata(pif.rdata_1),
+    .addr(pif.addr_1),
+    .ready(pif.ready_1));
   
   initial begin
-    uvm_config_db#(virtual mem_intf)::set(null,"*","pif_1",pif_1);
-    uvm_config_db#(virtual mem_intf)::set(null,"*","pif_2",pif_2);
-    #1000
-    $finish;
+    uvm_config_db#(virtual mem_intf)::set(null,"*","pif",pif);
+    //uvm_config_db#(virtual mem_intf)::set(null,"*","pif",pif);
+    //#1000
+    //$finish;
   end
     
   initial begin
@@ -60,7 +60,7 @@ module top;
     
   
   initial begin
-    run_test("mem_test");
+    run_test("mem_test_2");
   end
   
   initial begin
