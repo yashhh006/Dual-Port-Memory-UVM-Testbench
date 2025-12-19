77c6f0ab29e66d25afbe5c379193c449cd82a48e
Yash Patel <yashpatel6022@gmail.com>
2025-12-19 16:57:47 +0530

    mem_seq.sv

--- mem_seq.sv
+++ mem_seq.sv
@@ -22,3 +22,34 @@ class mem_seq extends uvm_sequence#(mem_tx);
     end
   endtask
 endclass
+
+class mem_seq_only_wr extends mem_seq;
+  `uvm_object_utils(mem_seq_only_wr)
+  `new_obj
+  
+  mem_tx tx;
+  
+  task body();
+    for(int i=0;i<`depth;i++)begin
+      `uvm_do_with(req,{req.write_1==1; req.addr_1==i;});
+    end
+    
+    //for(int i=0;i<`depth;i++)begin
+    //  `uvm_do_with(req,{req.write_1==0; req.addr_1==i;});
+    //end
+  endtask
+endclass
+
+class mem_seq_1wr extends mem_seq;
+  `uvm_object_utils(mem_seq_1wr);
+  `new_obj
+  
+  mem_tx tx;
+  
+  task body();
+    `uvm_do_with(req,{req.write_1==1; req.addr_1==0; req.wdata_1==5;});
+    `uvm_do_with(req,{req.write_1==1; req.addr_1==1; req.wdata_1==10;});
+    `uvm_do_with(req,{req.write_1==1; req.addr_1==2; req.wdata_1==15;});
+    `uvm_do_with(req,{req.write_1==1; req.addr_1==0; req.wdata_1==55;});
+  endtask
+endclass
